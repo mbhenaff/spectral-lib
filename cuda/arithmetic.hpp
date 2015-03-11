de937bd10b95a13807eadc3cd9b4d89b2ec2c1cd
Mikael Henaff <mbhenaff@vine10.cs.nyu.edu>
2015-03-11 19:50:31 -0400

    make compatible with fb torch

---
+++ cuda/arithmetic.hpp
@@ -0,0 +1,19 @@
+#ifndef __ARITHMETIC_H__
+#define __ARITHMETIC_H__
+
+// integer ceil
+#define DIVUP(a,b) (((a)+(b)-1)/(b))
+
+// fast integer power
+inline int ipow(int a, int n) {
+  if (n == 0)
+    return 1;
+  if (n == 1)
+    return a;
+  if (n & 1) // if ((i % 2) != 0)
+    return ipow(a, n-1) * a;
+  int b = ipow(a, n/2);
+  return b*b;
+}
+
+#endif
